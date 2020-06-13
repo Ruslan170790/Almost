80be276b2b1a367735a3cf5725b38449d717ce61
Admin <Admin@DESKTOP-EDJBDJ0>
2020-06-14 00:38:23 +0300

    Add project files.

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+namespace HelloApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the first number");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the second number");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+                if (num1 > num2)
+            {
+                Console.WriteLine("The first number is greater than the second");
+            }
+            else if (num1 < num2)
+            {
+                Console.WriteLine("The first number is less than the second");
+            }
+            else
+            {
+                Console.WriteLine("Both numbers are equal");
+            }
+            Console.ReadKey();
+        }
+    }
+}
+
+
